fd9169566a39d9e8988a5f61b08f2dc72b6a6b79
Christian Garbs <mitch@cgarbs.de>
2016-10-07 01:06:20 +0200

    move CornerPositionTest to correct package

--- src/test/java/de/cgarbs/roomplanner/room/corner/CornerPositionTest.java
+++ src/test/java/de/cgarbs/roomplanner/room/corner/CornerPositionTest.java
@@ -2,7 +2,7 @@
  * Copyright 2016 (C)  Christian Garbs <mitch@cgarbs.de>
  * Licensed under GNU GPL 3 (or later)
  */
-package de.cgarbs.roomplanner.corner;
+package de.cgarbs.roomplanner.room.corner;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
