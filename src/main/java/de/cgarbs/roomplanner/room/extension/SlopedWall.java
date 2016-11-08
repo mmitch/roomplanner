9038d52b6b916abc2805cc14bea225ffb01d9aae
Christian Garbs <mitch@cgarbs.de>
2016-11-08 23:13:05 +0100

    calculate wall area change on SlopedWall

--- src/main/java/de/cgarbs/roomplanner/room/extension/SlopedWall.java
+++ src/main/java/de/cgarbs/roomplanner/room/extension/SlopedWall.java
@@ -7,46 +7,68 @@ package de.cgarbs.roomplanner.room.extension;
 import de.cgarbs.roomplanner.length.Length;
 import de.cgarbs.roomplanner.room.ceiling.Ceiling;
 import de.cgarbs.roomplanner.room.floor.Floor;
+import de.cgarbs.roomplanner.room.wall.Wall;
 import de.cgarbs.roomplanner.room.wall.WallPosition;
 import de.cgarbs.roomplanner.room.wall.Walls;
 import de.cgarbs.roomplanner.shape.Rectangle;
-import de.cgarbs.roomplanner.shape.Shape;
+import de.cgarbs.roomplanner.shape.RightTriangle;
 
-public class SlopedWall implements Extension {
+public class SlopedWall implements Extension
+{
 
 	private WallPosition wall;
 	private Length heightOffset;
 	private Length depth;
 
-	public SlopedWall(WallPosition wall, Length heightOffset, Length depth) {
+	public SlopedWall(WallPosition wall, Length heightOffset, Length depth)
+	{
 		this.wall = wall;
 		this.heightOffset = heightOffset;
 		this.depth = depth;
 	}
 
 	@Override
-	public void extendFloor(Floor floor) {
+	public void extendFloor(Floor floor)
+	{
 		// floor remains unchanged
 	}
 
 	@Override
-	public void extendCeiling(Ceiling ceiling) {
+	public void extendCeiling(Ceiling ceiling)
+	{
 		ceiling.add(new Rectangle(getWallLength(ceiling), depth).negate());
 	}
 
 	@Override
-	public void extendWalls(Walls walls) {
-		// TODO Auto-generated method stub
+	public void extendWalls(Walls walls)
+	{
+		// shrink adjacent walls
+		wall.getAdjacentWalls().stream() //
+				.map((pos) -> walls.get(pos)) //
+				.forEach((wall) -> {
+					Length slopeHeight = wall.getHeight().add(heightOffset.negate());
+					RightTriangle triangle = new RightTriangle(slopeHeight, depth);
+					wall.add(triangle.negate());
+				});
+
+		// expand slope wall
+		Wall slopeWall = walls.get(wall);
+		Length slopeHeight = slopeWall.getHeight().add(heightOffset.negate());
+		RightTriangle triangle = new RightTriangle(slopeHeight, depth);
+		Length extraHeight = triangle.getC().add(slopeHeight.negate());
+		slopeWall.add(new Rectangle(extraHeight, slopeWall.getLength()));
 	}
 
-	private Length getWallLength(Ceiling ceiling) {
-		switch (wall) {
-		case EAST:
-		case WEST:
-			return ceiling.getNorthSouthLength();
-		case NORTH:
-		case SOUTH:
-			return ceiling.getEastWestLength();
+	private Length getWallLength(Ceiling ceiling)
+	{
+		switch (wall)
+		{
+			case EAST:
+			case WEST:
+				return ceiling.getNorthSouthLength();
+			case NORTH:
+			case SOUTH:
+				return ceiling.getEastWestLength();
 		}
 		return null; // FIXME: error handling
 	}
--- src/test/java/de/cgarbs/roomplanner/room/extension/SlopedWallTest.java
+++ src/test/java/de/cgarbs/roomplanner/room/extension/SlopedWallTest.java
@@ -5,38 +5,77 @@
 package de.cgarbs.roomplanner.room.extension;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 
 import de.cgarbs.roomplanner.room.ceiling.Ceiling;
 import de.cgarbs.roomplanner.room.floor.Floor;
+import de.cgarbs.roomplanner.room.wall.Wall;
 import de.cgarbs.roomplanner.room.wall.WallPosition;
+import de.cgarbs.roomplanner.room.wall.Walls;
 import de.cgarbs.roomplanner.test.stub.CM;
 import de.cgarbs.roomplanner.test.stub.CM2;
+import de.cgarbs.roomplanner.test.stub.M;
+import de.cgarbs.roomplanner.test.stub.M2;
 
-public class SlopedWallTest {
-
-	private SlopedWall slope = new SlopedWall(WallPosition.EAST, new CM(9), new CM(12));
-	
+public class SlopedWallTest
+{
 	@Test
-	public void slopedWallDoesNotChangeFloorArea() {
+	public void slopedWallDoesNotChangeFloorArea()
+	{
+		SlopedWall slope = new SlopedWall(WallPosition.SOUTH, new CM(2), new CM(2));
 		Floor floor = new Floor(new CM(2), new CM(3));
 		slope.extendFloor(floor);
 		assertThat(floor.getArea(), is(new CM2(6)));
 	}
 
 	@Test
-	public void slopedEastWallReducesCeilingAreaByNorthSouthWallLengthTimesDepth() {
+	public void slopedEastWallReducesCeilingAreaByNorthSouthWallLengthTimesDepth()
+	{
+		SlopedWall slope = new SlopedWall(WallPosition.EAST, new CM(9), new CM(12));
 		Ceiling ceiling = new Ceiling(new CM(10), new CM(20));
 		slope.extendCeiling(ceiling);
-		assertThat(ceiling.getArea(), is(new CM2(200 - 10*12)));
+		assertThat(ceiling.getArea(), is(new CM2(200 - 10 * 12)));
 	}
 
 	@Test
-	public void slopedEastWallIncreasesWallByComplicatedThinie() {
-		fail("FIXME: test for wall extension - not implemented");
+	public void slopedEastWallIncreasesWallArea()
+	{
+		/*
+		 * calculation is as follows:
+		 * 1) both adjacent walls get a triangle ABC cut out with
+		 * A~B = wall height - slope offset
+		 * B~C = slope depth
+		 * angle A<BC is a right angle, so one triangle is 1/2 * A~B * B~C
+		 * and both triangles areas together are A~B * B~C to be subtracted
+		 * from the wall area.
+		 * 2) on the same triangle, the wall from the slope offset on upwards
+		 * is slanted, A~B is smaller than A~C. Let D be on A~C so that
+		 * A~D = A~B. This makes D~C the additional slanted wall height to
+		 * be multiplied by the wall width.
+		 * A~C = sqrt( A~B² + B~C² ) and D~C = A~C - A~B, so
+		 * D~C = sqrt( A~B² + B~C² ) - A~B
+		 * So add D~C * wall length to the wall area.
+		 */
+
+		Walls walls = new Walls();
+		walls.put(WallPosition.SOUTH, new Wall(new M(6), new M(5)));
+		walls.put(WallPosition.WEST, new Wall(new M(8), new M(5)));
+		walls.put(WallPosition.NORTH, new Wall(new M(6), new M(5)));
+
+		// wall height is 5, so an offset of 2 gives A~B = 3
+		// B~C is 4 (depth)
+		// this conveniently makes A~C = sqrt( 3² + 4² ) = 5 and thus D~C = 2
+		SlopedWall slope = new SlopedWall(WallPosition.WEST, new M(2), new M(4));
+
+		slope.extendWalls(walls);
+
+		assertThat(walls.getArea(),
+				is(new M2( //
+						6 * 5 + 8 * 5 + 6 * 5 // initial area
+								- 3 * 4 // cutouts fron adjacent walls
+								+ 2 * 8 // additional slanted wall
+				)));
 	}
-	
-	// FIXME: tests for different wall directions (test an internal method?)
 }
