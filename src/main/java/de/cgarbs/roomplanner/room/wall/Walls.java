823c1d8f16761b7a16d09aebdd8934446f7a9588
Christian Garbs <mitch@cgarbs.de>
2016-11-21 22:50:26 +0100

    convert Walls to 3D faces

--- src/main/java/de/cgarbs/roomplanner/room/wall/Walls.java
+++ src/main/java/de/cgarbs/roomplanner/room/wall/Walls.java
@@ -6,12 +6,24 @@ package de.cgarbs.roomplanner.room.wall;
 
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import de.cgarbs.roomplanner.area.Area;
+import de.cgarbs.roomplanner.room.HasFaces;
+import de.cgarbs.wavefront.Face;
 
-public class Walls extends HashMap<WallPosition, Wall>
+public class Walls extends HashMap<WallPosition, Wall> implements HasFaces
 {
 	private static final long serialVersionUID = 1L;
+
+	public static Walls of(Wall... walls)
+	{
+		Walls w = new Walls();
+		Stream.of(walls).forEach(w::put);
+		return w;
+	}
 
 	public Area getArea()
 	{
@@ -27,6 +39,14 @@ public class Walls extends HashMap<WallPosition, Wall>
 	public void put(Wall wall)
 	{
 		put(wall.getPosition(), wall);
+	}
+
+	@Override
+	public List<Face> faces()
+	{
+		return values().stream() //
+				.flatMap((wall) -> wall.faces.stream()) //
+				.collect(Collectors.toList());
 	}
 
 }
---
+++ src/test/java/de/cgarbs/roomplanner/room/wall/WallsTest.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2016 (C)  Christian Garbs <mitch@cgarbs.de>
+ * Licensed under GNU GPL 3 (or later)
+ */
+package de.cgarbs.roomplanner.room.wall;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.Test;
+
+import de.cgarbs.roomplanner.pos.Position;
+import de.cgarbs.roomplanner.test.stub.CM;
+import de.cgarbs.roomplanner.test.stub.M;
+
+public class WallsTest
+{
+	@Test
+	public void wallsFacesAreFacesOfTheIndividualWallsConcatinated()
+	{
+		Wall wall1 = new Wall(WallPosition.NORTH, new CM(3), new CM(12), new Position(new M(2), new M(4)));
+		Wall wall2 = new Wall(WallPosition.WEST, new M(2), new M(7), new Position(new CM(12), new CM(14)));
+
+		Walls walls = Walls.of(wall1, wall2);
+
+		assertThat( //
+				walls.faces(),
+				is( //
+						Stream.of(wall1, wall2) //
+								.flatMap((wall) -> wall.faces.stream()) //
+								.collect(Collectors.toList())));
+	}
+}
